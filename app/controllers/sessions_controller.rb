074d103dab3264c17c2cae35bd674251cd8ceddb
LUIS ARCE <luisarce5@gmail.com>
2015-12-10 16:03:51 -0500

    Create Sessions_controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new #loging form
+  end
+
+  def create
+    # user puts in email and password
+    user = User.find_by_user_name(params[:user_name])
+    # do not be concerted about requiring strong parameters since we are not saving to database
+    if user && user.authenticate(params[:password])
+      # authenticate with bcrypt
+      session[:user_id] = user.id
+      redirect_to("/")
+
+    else
+      redirect_to("/login")
+    end
+  end
+
+  def destroy #logout
+    session[:user_id] = nil
+    redirect_to('/'
+  end
+
+end
